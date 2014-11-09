fc39d1aa76cf620310fa401fbd8d7128749126f1
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2014-11-09 20:44:29 +0100

    Init rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.order = :random
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+
+    mocks.verify_partial_doubles = true
+  end
+end
